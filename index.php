afb8f649af683512f9060b6f0af0f7d52050c73a
fifienalf <fifienalfinie@gmail.com>
2019-06-24 13:40:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Coba git remote</title>
 </head>
 <body>
-<h1>Hello World!</h1>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi excepturi ipsa, provident accusantium harum eveniet dolor. Quas, necessitatibus dolore! Perspiciatis eaque eos temporibus mollitia, repudiandae dolor architecto. Cum, quidem debitis.</p>
 </body>
 </html>
